6f6e92044e76fb806f31e5477a184720ff73e095
Tyler Bettilyon <teb311@gmail.com>
2015-12-27 20:41:42 -0700

    a few changes, but I think we need a new strategy here...

--- maybeTester.js
+++ maybeTester.js
@@ -20,24 +20,3 @@ function runTests(wordList){
 		console.log(word, trie.maybe(word, dictTrie) === tests[word]);
 	}
 }
-
-function loadDict(callback) {
-  var results = [];
-  var rl = require('readline').createInterface({
-    input: require('fs').createReadStream('/usr/share/dict/words')
-  });
-
-
-  // for every new line, if it matches the regex, add it to an array
-  // this is ugly regex :)
-  rl.on('line', function (line) {
-    results.push(line.toUpperCase());
-  });
-
-
-  // readline emits a close event when the file is read.
-  rl.on('close', function(){
-  	callback(results);
-  });
-}
-
--- shh.js
+++ shh.js
@@ -2,13 +2,12 @@ var trie = require('resig-trie');
 
 // GLOBALS
 var outcomes = {};
-var remainingEncountered = {};
+var attemptedSets;
 var dictTrie;
 var DICT;
 
 // Main entry
 loadDict(run);
-	
 
 function run(wordObj) {
 	var START = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
@@ -18,20 +17,18 @@ function run(wordObj) {
 	dictTrie = trie.create(Object.keys(wordObj));
 	console.log("created trie");
 
+	// create the set of winnable single word sets
+	attemptedSets = createWinnableSets(Object.keys(wordObj));
+	console.log("created winnable sets");
+
 	// START IT UP BRO
 	solveShh('', [], START, '');
+	printWinners();
 
 }
 
 function printWinners() {
-	for(play in outcomes) {
-		var cur = outcomes[play];
-
-		if(cur.win || (cur.inPlay + cur.unplayed).length < 4) {
-			console.log(cur);
-			console.log("======");
-		}
-	}
+	console.log(outcomes);
 }
 
 function solveShh(activeWord, verifiedWords, remainingLetters, letterOrder){
@@ -39,22 +36,9 @@ function solveShh(activeWord, verifiedWords, remainingLetters, letterOrder){
 	var outcomesCount = Object.keys(outcomes).length;
 	if(outcomesCount % 10000 === 0){
 		printWinners();
-		console.log(outcomesCount);
 	}
 
-	var remainingSorted = remainingLetters.split('').sort().join('');
-	var outcomeOfRemaining = remainingEncountered[remainingSorted];
-	if(outcomeOfRemaining !== undefined) {
-		outcomes[letterOrder] = {
-			win: outcomeOfRemaining.win,
-			words: verifiedWords,
-			unplayed: remainingSorted,
-			fastForward: true
-		}
-		return outcomes[letterOrder];
-	}
-
-	// If we've used all the letters
+	// If we've used all the letters, we need to finish
 	if(remainingLetters.length === 0){
 		if(activeWord === '') {
 			outcomes[letterOrder] = {
@@ -69,22 +53,28 @@ function solveShh(activeWord, verifiedWords, remainingLetters, letterOrder){
 				inPlay: activeWord
 			};
 		}
+
 		return outcomes[letterOrder];
 	}
 
-	// If there aren't words that can be created 
-	// with this prefix, return
-	if(trie.maybe(activeWord, dictTrie) === false) {
+	// Create the remaining sorted letters
+	var remainingSorted = remainingLetters.sort().join('');
+
+	// We build up this list of impossible situations
+	// as we run through the solver
+	if(attemptedSets[remainingSorted] === false) {
 		outcomes[letterOrder] = {
-			win: false,
-			words: verifiedWords, 
-			inPlay: activeWord, 
-			unplayed: remainingLetters
-		}
+				win: false, 
+				words: verifiedWords, 
+				inPlay: activeWord,
+				fastFail: true
+			};
+
 		return outcomes[letterOrder];
 	}
 
 	// Try each next letter brute force
+	var remainingIsUnwinnable = true;
 	for(i in remainingLetters) {
 		// track the new word
 		var currentLetter = remainingLetters[i];
@@ -97,6 +87,21 @@ function solveShh(activeWord, verifiedWords, remainingLetters, letterOrder){
 		var newLetters = remainingLetters.slice();
 		newLetters.splice(i, 1);
 
+		var sortedNew = newLetters.sort().join('');
+
+		// No words that can be created with newWord as the prefix
+		var failLookahead = trie.maybe(newWord, dictTrie) === false;
+		if(failLookahead) {
+			continue;
+		}
+
+		// if it's a word or not, try adding a letter
+		var continueOutcome = solveShh(newWord, verifiedWords, newLetters, curLetterOrder);
+		if(continueOutcome.win === true) {
+			remainingIsUnwinnable = false;
+			attemptedSets[sortedNew] = true;
+		}
+
 		// If the newWord is a word, then there are two
 		// paths, accept word and reject word.
 		if(DICT[newWord] !== undefined && newWord.length > 2) {
@@ -107,21 +112,38 @@ function solveShh(activeWord, verifiedWords, remainingLetters, letterOrder){
 			// add a + to letterOrder param to indicate alternate path
 			var newLetterOrder = curLetterOrder + '+';
 			var acceptOutcome = solveShh('', newWordList, newLetters, newLetterOrder);
+			
+			// If this results in a win, then this letter set is winnable!
+			if(acceptOutcome.win === true) {
+				remainingIsUnwinnable = false;
+				attemptedSets[sortedNew] = true;
+			}
 		}
-
-		// if it's a word or not, try adding a letter
-		var continueOutcome = solveShh(newWord, verifiedWords, newLetters, curLetterOrder);
+	}
+	// If, after trying all of the remaining letters, there has
+	// not been any win, then the sorted remaining letters must 
+	// be an unwinnable set
+	if(remainingIsUnwinnable && attemptedSets[remainingSorted] === undefined) {
+		attemptedSets[remainingSorted] = false;	
 	}
 
-	return {
-		win: false
-	}
+	 outcomes[letterOrder] = {
+		win: false,
+		words: verifiedWords,
+		unplayed: remainingSorted,
+		inPlay: activeWord
+	};
+	return outcomes[letterOrder];
 }
 
-function eqSet(as, bs) {
-    if (as.size !== bs.size) return false;
-    for (var a of as) if (!bs.has(a)) return false;
-    return true;
+function createWinnableSets(wordList) {
+	var winners = {};
+	for(i in wordList) {
+		var word = wordList[i];
+		var sorted = word.split('').sort().join('');
+		winners[sorted] = true;
+	}
+	return winners;
 }
 
 function loadDict(callback) {
