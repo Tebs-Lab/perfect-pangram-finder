920aeacf3a3cecee89c1d201f32459b1798ddd78
Tyler Bettilyon <teb311@gmail.com>
2016-05-30 10:38:53 -0700

    dynamic programming version of backtracking implemented. Now we store sub-pangrams instead of children pointers, which has dramatically increased the speed at which we find perfect pangrams.

--- findPangrams.js
+++ findPangrams.js
@@ -6,91 +6,89 @@ const util = require('./lib/utilities');
 util.loadDict(bootstrapSearch);
 
 function bootstrapSearch(wordList) {
-	// create the set of winnable single word sets
-	const COMPACT_DICT = util.createCompactDictionary(wordList);
-	console.log("created winnable sets");
+  // create the set of winnable single word sets
+  const COMPACT_DICT = util.createCompactDictionary(wordList);
+  console.log("created winnable sets");
 
-	const COMPACT_KEYS = Object.keys(COMPACT_DICT);
-	console.log(COMPACT_KEYS.length, " Total initial dictionary entries");
+  const COMPACT_KEYS = Object.keys(COMPACT_DICT);
+  console.log(COMPACT_KEYS.length, " Total initial dictionary entries");
 
-	const LETTER_FREQUENCY = util.constructFreqHistogram(COMPACT_KEYS);
-	console.log("constructed letter frequency");
+  const LETTER_FREQUENCY = util.constructFreqHistogram(COMPACT_KEYS);
+  console.log("constructed letter frequency");
 
-	const LETTER_SHARE = util.constructLetterShareHist(COMPACT_KEYS);
-	console.log("constructed letter share");
+  const LETTER_SHARE = util.constructLetterShareHist(COMPACT_KEYS);
+  console.log("constructed letter share");
 
 
-	// GLOBAL CONSTANTS
-	var MATCH_LENGTH = parseInt(process.argv[2]); // TODO: better argument handling.
-	if(!MATCH_LENGTH) MATCH_LENGTH = 0;
+  // GLOBAL CONSTANTS
+  var MATCH_LENGTH = parseInt(process.argv[2]); // TODO: better argument handling.
+  if(!MATCH_LENGTH) MATCH_LENGTH = 0;
 
-	var CONFIG = {
-		// Input Dictionary
-		COMPACT_DICT: COMPACT_DICT,
-		COMPACT_KEYS: COMPACT_KEYS,
+  var CONFIG = {
+    // Input Dictionary
+    COMPACT_DICT: COMPACT_DICT,
+    COMPACT_KEYS: COMPACT_KEYS,
 
-		// Dictionary Stats
-		LETTER_FREQUENCY: LETTER_FREQUENCY,
-		LETTER_SHARE: LETTER_SHARE,
+    // Dictionary Stats
+    LETTER_FREQUENCY: LETTER_FREQUENCY,
+    LETTER_SHARE: LETTER_SHARE,
 
-		// Printing
-		MATCH_LENGTH: MATCH_LENGTH,
-		VERBOSE: process.argv.indexOf('v') !== -1,
+    // Printing
+    MATCH_LENGTH: MATCH_LENGTH,
+    VERBOSE: process.argv.indexOf('v') !== -1,
 
-		// Word Choice Control
-		EXPLORATION_RATE: 0,
-		KNOWN_SOLUTIONS: [],
-		PREFERENCED_WORDS: []
-	};
-	if(process.argv.indexOf('--benchmark') !== -1) {
-		CONFIG.reporter = benchReporter;
-	}
+    // Word Choice Control
+    EXPLORATION_RATE: 0,
+  };
+  if(process.argv.indexOf('--benchmark') !== -1) {
+    CONFIG.reporter = benchReporter;
+  }
 
-	absoluteStart = Date.now();
-	search.search(COMPACT_KEYS, CONFIG);
+  absoluteStart = Date.now();
+  search.search(COMPACT_KEYS, CONFIG);
 }
 
 var absoluteStart;
 var snapshotEnd;
 var prevExploredCount;
-function benchReporter(explored, frontier, CONFIG) {
-	// Report every some odd nodes
-	let curExploredSize = Object.keys(explored).length;
-	let exploredThisTime = curExploredSize - prevExploredCount;
-	if(exploredThisTime < 1000) {
-		return;
-	}
-	prevExploredCount = curExploredSize;
+function benchReporter(explored, frontier, rootNode, CONFIG) {
+  // Report every some odd nodes
+  let curExploredSize = Object.keys(explored).length;
+  let exploredThisTime = curExploredSize - prevExploredCount;
+  if(exploredThisTime < 1000) {
+    return;
+  }
+  prevExploredCount = curExploredSize;
 
-	snapshotEnd = Date.now();
-	let secondsElappsed = (snapshotEnd - absoluteStart) / 1000;
+  snapshotEnd = Date.now();
+  let secondsElappsed = (snapshotEnd - absoluteStart) / 1000;
 
-	console.log("============");
-	console.log(`Seconds Elappsed:            ${secondsElappsed}`);
-	console.log(`Nodes explored per solution: ${curExploredSize / CONFIG.KNOWN_SOLUTIONS.length}`);
-	console.log(`Explored per second:         ${curExploredSize / secondsElappsed}`);
-	console.log(`Explored total:              ${curExploredSize}`);
-	console.log(`Solutions total:             ${CONFIG.KNOWN_SOLUTIONS.length}`);
-	console.log(`Current frontier size:       ${frontier.size()}`);
+  console.log("============");
+  console.log(`Seconds Elappsed:            ${secondsElappsed}`);
+  console.log(`Nodes explored per solution: ${curExploredSize / rootNode.subPangrams.length}`);
+  console.log(`Explored per second:         ${curExploredSize / secondsElappsed}`);
+  console.log(`Explored total:              ${curExploredSize}`);
+  console.log(`Solutions total:             ${rootNode.subPangrams.length}`);
+  console.log(`Current frontier size:       ${frontier.size()}`);
 
-	// Dump frontier stats
-	if(CONFIG.VERBOSE) {
-		console.log('---- frontier decile avgs ----');
-		let frontElms = frontier._elements;
-		let decileSize = Math.floor(frontElms.length / 10);
-		let currentDecileUtil = 0;
-		let currentDecileLen = 0;
-		// (i = 1 to ignore single worst for mod convinence)
-		for(let i = 1; i < frontElms.length; i++) {
-			let node = frontElms[i];
-			currentDecileLen += node.letters.length;
-			currentDecileUtil += node.utility;
+  // Dump frontier stats
+  if(CONFIG.VERBOSE) {
+    console.log('---- frontier decile avgs ----');
+    let frontElms = frontier._elements;
+    let decileSize = Math.floor(frontElms.length / 10);
+    let currentDecileUtil = 0;
+    let currentDecileLen = 0;
+    // (i = 1 to ignore single worst for mod convinence)
+    for(let i = 1; i < frontElms.length; i++) {
+      let node = frontElms[i];
+      currentDecileLen += node.letters.length;
+      currentDecileUtil += node.utility;
 
-			if(i % decileSize === 0) {
-				console.log(`L: ${currentDecileLen / decileSize}, U: ${currentDecileUtil / decileSize}`);
-				currentDecileUtil = 0;
-				currentDecileLen = 0;
-			}
-		}
-	}
+      if(i % decileSize === 0) {
+        console.log(`L: ${currentDecileLen / decileSize}, U: ${currentDecileUtil / decileSize}`);
+        currentDecileUtil = 0;
+        currentDecileLen = 0;
+      }
+    }
+  }
 }
